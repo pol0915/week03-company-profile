3edf2cadb823bc14d29cc517e6885c9519562237
Paul Andrey Roldan <paulandreyabadroldan@gmail.com>
2026-08-14 22:54:36 +0800

    feat: create CompanyController

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class CompanyController extends Controller
+{
+    public function home(): View
+    {
+        return view('pages.home');
+    }
+
+    public function about(): View
+    {
+        return view('pages.about');
+    }
+
+    public function services(): View
+    {
+        return view('pages.services');
+    }
+
+    public function contact(): View
+    {
+        return view('pages.contact');
+    }
+}
